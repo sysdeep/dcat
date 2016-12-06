8841be7b93b7a94629e0bba9499642651d511022
Diver - home(win) <sysdeep@yandex.ru>
2016-12-06 20:58:17 +0300

    add walker

---
+++ app/logic/walker.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+
+
+class Walker(object):
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+
+
+
+    def set_path(self, folder_path):
+        """"""
+        self.folder_path = folder_path
+
+
+
+    def start(self):
+        print("start")
---
+++ app/rc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os.path
+
+DIR_SELF = os.path.dirname(os.path.abspath(__file__))
+
+
+
+
+def get(s):
+    s=3
+    return s
+
+
+class aaa(object):
+    def __init__():
+        self.sss = None
