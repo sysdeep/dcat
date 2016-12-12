44540088632f2d0c3a0bd5b71480cd153919d9a6
Diver - home(win) <sysdeep@yandex.ru>
2016-12-12 20:00:13 +0300

    test dyn tree

--- app/gui/Tree.py
+++ app/gui/Tree.py
@@ -24,11 +24,13 @@ class Tree(QTreeView):
 		# self.setHeaderHidden(True)
 		self.setFixedWidth(300)
 
+		self.__rows_dict = {}
 		self.tree = get_tree()
 
 		events.on("update_tree", self.__update_tree)
 
 		self.clicked.connect(self.__select)
+		self.expanded.connect(self.__expanded_item)
 
 
 	def __update_tree(self):
@@ -48,14 +50,17 @@ class Tree(QTreeView):
 		root_items = root.childrens
 
 		for item in root_items:
-			self.__wnode(item, self.model)
+			self.__wnode(item, self.model, 0)
 
 
 
-	def __wnode(self, node, parent):
+	def __wnode(self, node, parent, deep):
 			"""
 				рекурсивный обход элементов и добавление их на форму
 			"""
+			# if deep == 2:
+			# 	return
+
 			if node.ntype == "f":
 				# icon = QIcon(get_icon_path("document-properties.png"))
 				icon = qicon("empty.png")
@@ -73,6 +78,8 @@ class Tree(QTreeView):
 
 			row.setData(node.id, Qt.UserRole+1)
 
+			self.__rows_dict[node.id] = row
+
 			parent.appendRow(row)							# добавляем
 
 
@@ -86,7 +93,7 @@ class Tree(QTreeView):
 
 			#--- для каждого из деток вызываем рекурсию
 			for node in child_items:
-				self.__wnode(node, row)
+				self.__wnode(node, row, deep+1)
 
 
 	def __select(self, index):
@@ -98,3 +105,21 @@ class Tree(QTreeView):
 			self.select_cb(selected_id)
 
 
+
+
+
+
+
+	def __expanded_item(self, model_index):
+		selected_id = model_index.data(Qt.UserRole+1)
+		print(selected_id)
+
+		# node = self.tree.get_node_id(selected_id)
+		# # print(node)
+
+		
+		
+
+		# for x in node.childrens:
+		# 	tree_branch = self.__rows_dict[x.id]
+		# 	self.__wnode(x, tree_branch, 0)
