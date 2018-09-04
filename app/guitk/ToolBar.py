5624de33f824d5191eb47a8e1ab0315c3bfbf2eb
Diver Dune <sysdeep@yandex.ru>
2018-09-04 15:12:12 +0300

    return flat toolbar

--- app/guitk/ToolBar.py
+++ app/guitk/ToolBar.py
@@ -14,46 +14,52 @@ class ToolBar(ttk.Frame):
 		self.cb_open_db = None
 		self.cb_create_db = None
 		self.cb_create_db_backup = None
+		self.cb_show_find_modal = None
+
+
+		self.buttons = (
+			ToolBarButton(self, text="Открыть", image=ticons.ticon(ticons.I_OPEN_FILE), compound="left", command=self.__open_db),
+			ToolBarButton(self, text="Создать", command=self.__create_db, image=ticons.ticon(ticons.I_CREATE_FILE), compound="left"),
+			ToolBarButton(self, text="Добавить том", command=self.__show_add_volume, image=ticons.ticon(ticons.I_ADD_ITEM), compound="left"),
+			ToolBarButton(self, text="Импортировать том", command=self.__show_import_volume, image=ticons.ticon(ticons.I_IMPORT), compound="left"),
+			ToolBarButton(self, text="BackUP", command=self.__create_db_backup, image=ticons.ticon(ticons.I_SAVE_AS), compound="left"),
+			ToolBarButton(self, text="Поиск", command=self.__show_find_modal, image=ticons.ticon(ticons.I_FIND), compound="left"),
+		)
 
 
 
 
-		# self.btn_open = tkinter.Button(self, text="Открыть", command=self.__open_db, image=ticons.ticon(ticons.I_OPEN_FILE), relief="flat", compound="left")
-		self.btn_open = ttk.Button(self, text="Открыть", command=self.__open_db, image=ticons.ticon(ticons.I_OPEN_FILE), compound="left")
+		for btn in self.buttons:
+			btn.pack(side="left", ipadx=10, ipady=10)
+
+
+
+		# # self.btn_open = tkinter.Button(self, text="Открыть", command=self.__open_db, image=ticons.ticon(ticons.I_OPEN_FILE), relief="flat", compound="left")
+		# # self.btn_open = ttk.Button(self, text="Открыть", command=self.__open_db, image=ticons.ticon(ticons.I_OPEN_FILE), compound="left")
 		# self.btn_open = ttk.Button(self, command=self.__open_db, image=ticons.ticon(ticons.I_OPEN_FILE), compound="left")
-		self.btn_open.pack(side="left")
+		# self.btn_open.pack(side="left")
 
 		# self.btn_new = tkinter.Button(self, text="Создать", command=self.__create_db, image=ticons.ticon(ticons.I_CREATE_FILE), relief="flat", compound="left")
-		self.btn_new = ttk.Button(self, text="Создать", command=self.__create_db, image=ticons.ticon(ticons.I_CREATE_FILE), compound="left")
-		self.btn_new.pack(side="left")
+		# self.btn_new = ttk.Button(self, text="Создать", command=self.__create_db, image=ticons.ticon(ticons.I_CREATE_FILE), compound="left")
+		# self.btn_new.pack(side="left")
 
 		# btn_add_volume = tkinter.Button(self, text="Добавить том", command=self.__show_add_volume, image=ticons.ticon(ticons.I_ADD_ITEM), relief="flat", compound="left")
-		btn_add_volume = ttk.Button(self, text="Добавить том", command=self.__show_add_volume, image=ticons.ticon(ticons.I_ADD_ITEM), compound="left")
-		btn_add_volume.pack(side="left")
+		# btn_add_volume = ttk.Button(self, text="Добавить том", command=self.__show_add_volume, image=ticons.ticon(ticons.I_ADD_ITEM), compound="left")
+		# btn_add_volume.pack(side="left")
 
 		# btn_import_volume = tkinter.Button(self, text="Импортировать том", command=self.__show_import_volume, image=ticons.ticon(ticons.I_IMPORT), relief="flat", compound="left")
-		btn_import_volume = ttk.Button(self, text="Импортировать том", command=self.__show_import_volume, image=ticons.ticon(ticons.I_IMPORT), compound="left")
-		btn_import_volume.pack(side="left")
+		# btn_import_volume = ttk.Button(self, text="Импортировать том", command=self.__show_import_volume, image=ticons.ticon(ticons.I_IMPORT), compound="left")
+		# btn_import_volume.pack(side="left")
+		#
+		# # self.btn_backup = tkinter.Button(self, text="BackUP", command=self.__create_db_backup, image=ticons.ticon(ticons.I_SAVE_AS), relief="flat", compound="left")
+		# self.btn_backup = ttk.Button(self, text="BackUP", command=self.__create_db_backup, image=ticons.ticon(ticons.I_SAVE_AS), compound="left")
+		# self.btn_backup.pack(side="left")
+		#
+		# # btn_find = tkinter.Button(self, text="Поиск", command=self.__show_find_modal, image=ticons.ticon(ticons.I_FIND), relief="flat", compound="left")
+		# btn_find = ttk.Button(self, text="Поиск", command=self.__show_find_modal, image=ticons.ticon(ticons.I_FIND), compound="left")
+		# btn_find.pack(side="left")
 
-		# self.btn_backup = tkinter.Button(self, text="BackUP", command=self.__create_db_backup, image=ticons.ticon(ticons.I_SAVE_AS), relief="flat", compound="left")
-		self.btn_backup = ttk.Button(self, text="BackUP", command=self.__create_db_backup, image=ticons.ticon(ticons.I_SAVE_AS), compound="left")
-		self.btn_backup.pack(side="left")
 
-		# btn_find = tkinter.Button(self, text="Поиск", command=self.__show_find_modal, image=ticons.ticon(ticons.I_FIND), relief="flat", compound="left")
-		btn_find = ttk.Button(self, text="Поиск", command=self.__show_find_modal, image=ticons.ticon(ticons.I_FIND), compound="left")
-		btn_find.pack(side="left")
-
-
-		# i = ttk.Frame(self, borderwidth=2, padding=10, cursor="hand2")
-		# i.bind("<Button-1>", lambda x: print("click"))
-		
-		# i.pack()
-
-		# lqqq = ttk.Label(i, image=ticons.ticon(ticons.I_IMPORT), compound="left")
-		# lqqq.bind("<Button-1>", lambda x: print("laabel click"))
-		# # lqqq.bind("<Enter>", lambda x: print("enter"))
-		# # lqqq.bind("<Leave>", lambda x: print("leave"))
-		# lqqq.pack()
 
 		
 
@@ -72,7 +78,8 @@ class ToolBar(ttk.Frame):
 
 
 	def __show_find_modal(self):
-		self.cb_show_find_modal()
+		if self.cb_show_find_modal:
+			self.cb_show_find_modal()
 
 
 	def __show_add_volume(self):
@@ -83,3 +90,25 @@ class ToolBar(ttk.Frame):
 		dbus.emit(dbus.SHOW_IMPORT_VOLUME)
 
 
+
+
+
+
+
+
+
+
+class ToolBarButton(ttk.Label):
+	def __init__(self, master, command=None, **kwargs):
+		super(ToolBarButton, self).__init__(master, **kwargs)
+
+		self.command = command
+
+		self.bind("<Button-1>", self.__on_click)
+
+	def __on_click(self, event):
+		if self.command:
+			try:
+				self.command()
+			except:
+				pass
