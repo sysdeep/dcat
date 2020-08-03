7b593300d6b090e627aa7934c0e99ad8763d913c
Diver Dune <sysdeep@yandex.ru>
2020-08-03 13:53:33 +0300

    test bss v1

--- app/storage/export/Bss.py
+++ app/storage/export/Bss.py
@@ -6,93 +6,66 @@ import base64
 import struct
 from app.lib import tools
 
-def __kv(key, value):
-	return "{}:{}\n".format(key, value)
+
+MAGIC = 0xfafb
+VERSION = 1
 
 
-def __str2base(data: str) -> str:
-	return base64.standard_b64encode(data.encode()).decode("utf-8")
+def __ushort2(v) -> bytes:
+	return struct.pack("<H", v)
 
+def __uint4(v) -> bytes:
+	return struct.pack("<I", v)
 
+def __ulong8(v) -> bytes:
+	return struct.pack("<Q", v)
 
 def __pack_str(text: str):
 	bstr = text.encode(encoding="utf-8")
 	str_len = len(bstr)
-	bdata = struct.pack("<I", str_len)
+	bdata = __ushort2(str_len)
 	bdata += bstr
 	return bdata
 
 
 
-def __vol_info(data: dict):
+
+
+
+
+def __vol_info(data: dict, records_len: int):
 	"""
 	{'created': '2017-09-27 11:05:42', 'name': 'oxygen_16x16', 'description': None,
 	'updated': None, 'uuid': '3a618114-f766-4767-a058-79d3a1b1da07', 'vtype': 'cd', 'path': '/home/nia/Development/_Python/_DCat/oxygen_16x16'}
 	
-	
-	313057 - 9.1m
-	replace 313057 - 12s - 10.2m
-	base64 313057 - 3.3s - 13.2m
-	
-	
-	
-	full_len
-	version
-	created
-	icon
-	name
-	description
-	
 	"""
 	
-	# safe_description = data["description"].replace("\n", "___LF___").replace(":", "___TERM___")
-	# safe_name = data["name"]
-	#
-	# # safe_description = __str2base(data["description"])
-	# # safe_name = __str2base(data["name"])
-	#
-	#
-	# result = ""
-	# result += __kv("version", 1)
-	# result += __kv("name", safe_name)
-	# # result += __kv("name", data["name"])
-	# # result += __kv("description", data["description"])
-	# result += __kv("description", safe_description)
-	# # result += __kv("description", re.escape(data["description"]))
-	# # result += __kv("description", data["description"].encode())
-	# result += __kv("created", data["created"])
-	# result += __kv("vtype", data["vtype"])
-	#
-	#
-	#
+	bdata = b''
 	
+	#--- [created](8)
+	bdata += __ulong8(0)				# TODO
 	
+	#--- [icon](2)
+	bdata += __ushort2(1)				# TODO
 	
-	dataset = (
-		1,						# 0 - version
-		1234,					# 1 - created
-		1,						# 2 - icon
-		# data["name"],			# 3 - name
-		# data["description"], 	# 4 - description
-		
-		
-	)
+	#--- [records](8)
+	bdata += __ulong8(records_len)
 	
-	bpre = struct.pack("<III", *dataset)
+	#--- [name]
+	bdata += __pack_str(data["name"])
 	
-	bname = __pack_str(data["name"])
-	bdescription = __pack_str(data["description"])
+	#--- [scan_path]
+	bdata += __pack_str(data["path"])
 	
-	bdata_pre = bpre + bname + bdescription
-	
-	len_header = len(bdata_pre)
-	
-	bdata = struct.pack("<I", len_header)
-	bdata += bdata_pre
-	
-	
+	#--- [description]
+	bdata += __pack_str(data["description"])
 	
 	return bdata
+
+
+
+
+
 
 
 def __format_row(row_data: dict) -> bytes:
@@ -111,63 +84,37 @@ def __format_row(row_data: dict) -> bytes:
 	
 	"""
 	
-	# safe_description = row_data["description"].replace("|", ":")
-	# safe_name = row_data["name"].replace("|", ":")
-	# safe_parent_id = row_data["parent_id"]
-	# safe_uuid = row_data["uuid"]
-	#
-	# # safe_description = __str2base(row_data["description"])
-	# # safe_name = __str2base(row_data["name"])
-	# # safe_parent_id = __str2base(row_data["parent_id"])
-	# # safe_uuid = __str2base(row_data["uuid"])
-	#
-	# dataset = (
-	# 	# "'" + row_data["name"] + "'",
-	# 	safe_name,							# 0
-	# 	str(row_data["size"]),										# 1
-	# 	str(row_data["ctime"]),										# 2
-	# 	str(row_data["mtime"]),										# 3
-	# 	str(row_data["atime"]),										# 4
-	# 	str(row_data["rights"]),									# 5
-	# 	row_data["group"],											# 6
-	# 	row_data["owner"],											# 7
-	# 	safe_description,					# 8
-	# 	safe_parent_id,										# 9
-	# 	safe_uuid,											# 10
-	# )
-	#
-	# result = "|".join(dataset)
-	#
-	#
-	#
+	bdata = b''
 	
-	# return result + "\n"
+	#--- type 			[ushort 2]	- тип файла(каталог/файл...)
+	bdata += __ushort2(1)				# TODO
+	
+	#--- size 			[ulong 8]	- размер записи
+	bdata += __ulong8(row_data["size"])
+	
+	#--- ctime 		[ulong 8]	- дата создания файла(unix timestamp)
+	bdata += __ulong8(0)				# TODO
+	
+	#--- rights 		[ushort 2]	- код доступа(unix 777)
+	bdata += __ushort2(row_data["rights"])
+	
+	#--- fid 			[uint 4]	- id записи
+	bdata += __uint4(0)					# TODO
+	
+	#--- pid 			[uint 4]	- id родителя(0 - корень)
+	bdata += __uint4(0)					# TODO
+	
+	#--- name 			[bstr]		- название
+	bdata += __pack_str(row_data["name"])
+	
+	#--- description 	[bstr]		- произвольное описание
+	bdata += __pack_str(row_data["description"])
 
-	dataset = (
-		row_data["size"],			# size
-		row_data["ftype"],			#
-		row_data["ctime"],			#
-		row_data["mtime"],
-		row_data["atime"],
-		row_data["rights"],
-		
-	)
 	
-	bints = struct.pack("<IIIIII", *dataset)
-	bname = __pack_str(row_data["name"])
-	bdescription = __pack_str(row_data["description"])
-	bparent_id = __pack_str(row_data["parent_id"])
-	buuid = __pack_str(row_data["uuid"])
-	bgroup = __pack_str(row_data["group"])
-	bowner = __pack_str(row_data["owner"])
-	
-	pre_bdata = bints + bname + bdescription + bparent_id + buuid + bgroup + bowner
-	
-	len_bdata = len(pre_bdata)
-	
-	bdata = struct.pack("<I", len_bdata)
-	bdata += pre_bdata
 	return bdata
+
+
+
 
 @tools.dtimeit
 def start_export(vol_info: dict, files_list: list, file_path: str):
@@ -175,16 +122,47 @@ def start_export(vol_info: dict, files_list: list, file_path: str):
 	
 	
 	fd = gzip.open(file_path + ".gz", "wb")
-	header = __vol_info(vol_info)
+	
+	magic = __ushort2(MAGIC)
+	#--- [magic](2)
+	fd.write(magic)
+	
+	#--- [version](2)
+	fd.write(__ushort2(VERSION))
+	
+	
+	header = __vol_info(vol_info, len(files_list))
 	print("header len: ", len(header))
+	#--- [header_len](2)
+	fd.write(__ushort2(len(header)))
+	
+	#--- [header_struct]
 	fd.write(header)
-	# fd.write("-"*10)
-	# fd.write("\n")
-	#
+	
+	#--- [magic](2)
+	fd.write(magic)
+	
 	for fdata in files_list:
-	# 	# print(__format_row(fdata))
-		fd.write(__format_row(fdata))
-	#
+		
+		try:
+			row_data = __format_row(fdata)
+		except Exception as e:
+			print(e)
+			print("+"*10)
+			print(fdata)
+			print("+"*10)
+			break
+		
+		#--- [row_len]
+		fd.write(__ushort2(len(row_data)))
+		
+		#--- [row_struct]
+		fd.write(row_data)
+	
+	#--- [magic](2)
+	fd.write(magic)
+	
+	
 	fd.flush()
 	fd.close()
 	
--- documentation/Bss_format-v1.md
+++ documentation/Bss_format-v1.md
@@ -7,9 +7,10 @@
 
 ## Форматы данных
 
-- uint 4 - unsigned int 4 bytes (I)(2 147 483 647)
-- ushort 2 - unsigned short 2 bytes (H)(65535)
-- bstr - счётная строка [[str_len - uint 4][binary string - str_len]]
+- uint 4 - unsigned int 4 bytes (<I)(2 147 483 647)
+- ushort 2 - unsigned short 2 bytes (<H)(65 535)
+- ulong 8 - unsigned long long 8 bytes (<Q)(18 446 744 073 709 551 615)
+- bstr - счётная строка [[str_len - ushort][binary string - str_len]]
 
 
 
@@ -20,10 +21,10 @@
 
 [magic][version][header_len][header_struct][magic][row_len][row_struct]...[row_len][row_struct][magic]
 
-- magic 		[uint 4] - идентификатор файла и метка контроля структуры
-- version 		[uint 4] - версия структуры файла
-- header_len 	[uint 4] - длина данных структуры заголовка
-- row_len		[uint 4] - длина данных структуры строки данных файла
+- magic 		[ushort 2] - идентификатор файла и метка контроля структуры
+- version 		[ushort 2] - версия структуры файла
+- header_len 	[ushort 2] - длина данных структуры заголовка
+- row_len		[ushort 2] - длина данных структуры строки данных файла
 - header_struct - структура заголовка
 - row_struct - структура строки данных
 
@@ -40,7 +41,7 @@
 
 идентификатор файла - заданная константа для проверки типа файла
 
-198411121
+0xfafb(64251)
 
 
 
@@ -51,9 +52,9 @@
 
 [created][icon][records][name][scan_path][description]
 
-- created 		[uint 4]	- дата создания(unix timestamp)
-- icon 			[uint 4]	- id иконки
-- records  		[uint 4] 	- кол-во записей
+- created 		[ulong 8]	- дата создания(unix timestamp)
+- icon 			[ushort 2]	- id иконки
+- records  		[ulong 8] 	- кол-во записей
 - name 			[bstr]		- название тома
 - scan_path		[bstr]		- путь до ресурса сканирования
 - description 	[bstr]		- произвольное описание
@@ -70,21 +71,21 @@
 
 ## Структура Записи данных - row_struct
 
-[type][size][ctime][rights][fid][pid][reserv][reserv][name][description]
+[type][size][ctime][rights][fid][pid][name][description]
 
 - type 			[ushort 2]	- тип файла(каталог/файл...)
-- size 			[uint 4]	- размер записи
-- ctime 		[uint 4]	- дата создания файла(unix timestamp)
+- size 			[ulong 8]	- размер файла
+- ctime 		[ulong 8]	- дата создания файла(unix timestamp)
 - rights 		[ushort 2]	- код доступа(unix 777)
 - fid 			[uint 4]	- id записи
 - pid 			[uint 4]	- id родителя(0 - корень)
-- reserv 		[uint 4]	- резерв
 - name 			[bstr]		- название
 - description 	[bstr]		- произвольное описание
 
 
 Эти не пригодились ещё нигде...
 
+- reserv 		[uint 4]	- резерв
 - owner 		?[uint 4]	- владелец
 - group			?[uint 4]	- группа
 
